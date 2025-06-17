45cc5efef5854d7ff3bcca78e86924b3888c83a6
swichaja <68030291@kmitl.ac.th>
2025-06-17 07:27:53 +0000

    9คอล2

---
+++ cell02/ex00/ex03/mult.py
@@ -0,0 +1,7 @@
+num1 =int(input("Enter the first nunber"))
+num2 = int (input("Enter the second nunber"))
+print(f"nu{num1}x{num2}={num1*num2}")
+if num1 * num2 > 0:
+   print("The result is negative.")
+else:
+  print("The result is positive and negative")
