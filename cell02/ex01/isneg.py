23ed845e466742ea5fd0f7de70d8f10a2d60ca83
swichaja <68030291@kmitl.ac.th>
2025-06-17 05:09:15 +0000

    นัมเบอ2

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,7 @@
+number = int(input("enter a number:"))
+if number > 0:
+   print ("This number is positive.")
+elif number < 0:
+     print ("this number is negtive.")
+else : 
+    print ("This number is both positive ana negative.")
