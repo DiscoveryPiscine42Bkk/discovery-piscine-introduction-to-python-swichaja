c11d0202e17b9645e7e46fca7e3b0604ea7881cc
swichaja <68030291@kmitl.ac.th>
2025-06-17 07:52:02 +0000

    call3 ex00

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,11 @@
+from binascii import Error
+
+from torch import t
+
+
+value = int(input())
+if value >= 25:
+    print(Error)
+else:
+  for num in range(value,25):
+print(f"Inside the loop,my variable is {num}")
